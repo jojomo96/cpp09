e27cecfb6ebabcb080a0f3daa4b5fb81de0239b5
jojomo96 <48690914+jojomo96@users.noreply.github.com>
2025-01-09 00:19:49 +0100

    update calculation of jacobsthal

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,21 +6,22 @@
 #include <cmath>
 
 
-Element::Element(int data, const int depth) : _data(data),  _depth(depth) {
+Element::Element(int data, const int depth) : _data(data), _depth(depth) {
 }
 
 Element::Element(const std::shared_ptr<Element> &first, const std::shared_ptr<Element> &second, const int depth)
 	: _data(Pair(first, second)), _depth(depth) {
 }
 
-int Element::getMaxValue() const { // NOLINT(*-no-recursion)
+int Element::getMaxValue() const {
+	// NOLINT(*-no-recursion)
 	if (std::holds_alternative<int>(_data)) {
 		return std::get<int>(_data); // Base Case
 	}
 
 	// Validate Pair data before proceeding
 	if (std::holds_alternative<Pair>(_data)) {
-		const auto& [first, second] = std::get<Pair>(_data);
+		const auto &[first, second] = std::get<Pair>(_data);
 
 		// Ensure pointers are valid
 		if (!first || !second) {
@@ -36,21 +37,22 @@ int Element::getMaxValue() const { // NOLINT(*-no-recursion)
 }
 
 
-void Element::print(const int i) const { // NOLINT(*-no-recursion)
-	static constexpr std::array<const char*, 6> colors = {
+void Element::print(const int i) const {
+	// NOLINT(*-no-recursion)
+	static constexpr std::array<const char *, 6> colors = {
 		"\033[0;31m", // Red
 		"\033[0;32m", // Green
 		"\033[0;33m", // Yellow
 		"\033[0;34m", // Blue
 		"\033[0;35m", // Magenta
-		"\033[0;36m"  // Cyan
+		"\033[0;36m" // Cyan
 	};
 	static auto resetColor = "\033[0m";
 
 	if (std::holds_alternative<int>(_data)) {
 		std::cout << colors[i % colors.size()] << std::get<int>(_data) << resetColor << " ";
 	} else if (std::holds_alternative<Pair>(_data)) {
-		const auto& [first, second] = std::get<Pair>(_data);
+		const auto &[first, second] = std::get<Pair>(_data);
 
 		// Validate pointers
 		if (!first || !second) {
@@ -59,8 +61,8 @@ void Element::print(const int i) const { // NOLINT(*-no-recursion)
 		}
 
 		std::cout << colors[i % colors.size()] << "( ";
-		first->print( i);
-		second->print( i);
+		first->print(i);
+		second->print(i);
 		std::cout << colors[i % colors.size()] << ")" << resetColor;
 	} else {
 		std::cerr << colors[i % colors.size()] << "Unsupported type in _data" << resetColor << "\n";
@@ -82,7 +84,7 @@ void Element::sortElement() {
 	}
 
 	if (std::holds_alternative<Pair>(_data)) {
-		auto& [first, second] = std::get<Pair>(_data);
+		auto &[first, second] = std::get<Pair>(_data);
 
 		if (!first || !second) {
 			throw std::runtime_error("Invalid Pair: Null pointer detected");
@@ -100,15 +102,15 @@ inline int PmergeMe::getGlobalComparisonCount() {
 	return globalComparisonCount;
 }
 
-int PmergeMe::runVector(const std::vector<int>& input) {
-	std::vector<std::shared_ptr<Element>> elements;
+int PmergeMe::runVector(const std::vector<int> &input) {
+	std::vector<std::shared_ptr<Element> > elements;
 	parseInput(elements, input);
 	run(elements);
 	return globalComparisonCount;
 }
 
 int PmergeMe::runDeque(const std::vector<int> &input) {
-	std::deque<std::shared_ptr<Element>> elements;
+	std::deque<std::shared_ptr<Element> > elements;
 	parseInput(elements, input);
 	run(elements);
 	return globalComparisonCount;
@@ -124,59 +126,28 @@ std::vector<size_t> generateJacobsthalUpTo(size_t maxN) {
 		result.push_back(jacobsthalNumber);
 	}
 
-	if (result.size() > 1 && result[0] == 1 && result[1] == 1) {
-		result.erase(result.begin());
-	}
-
 	return result;
 }
 
 std::vector<size_t> PmergeMe::generateJacobsthalIndices(const size_t listSize) {
-	if (listSize < 1) {
-		return {};
-	}
+	if (listSize < 1) return {};
 
 	const std::vector<size_t> jNumbers = generateJacobsthalUpTo(listSize);
 
-	std::cout << "Jacobsthal numbers: ";
-	for (const auto& j: jNumbers) {
-		std::cout << j << " ";
-	}
-	std::cout << std::endl;
+	if (jNumbers.empty()) return {};
+
+	auto jCurrentIt = jNumbers.begin() + 1;
+	auto jPreviousIt = jNumbers.begin();
+
 	std::vector<size_t> indices;
-
-	if (jNumbers.empty()) {
-		return { };
-	}
-
-	if (jNumbers.size() == 1) {
-		return { 0, 1 };
-	}
-
-	indices.push_back(1);
-	indices.push_back(0);
-
-	if (jNumbers.size() == 2) {
-		return indices;
-	}
-
-	auto jCurrentIt = jNumbers.begin() + 2;
-	auto jPreviousIt = jNumbers.begin() + 1;
-
 	while (jCurrentIt != jNumbers.end()) {
-		const size_t diff = *jCurrentIt - *jPreviousIt;
-
-		std::cout << "jCurrent: " << *jCurrentIt << ", jPrevious: " << *jPreviousIt << ", diff: " << diff << std::endl;
-
-		for (size_t i = 0; i < diff; ++i) {
+		for (size_t i = 0; i < *jCurrentIt - *jPreviousIt; ++i) {
 			indices.push_back(*jCurrentIt - i - 2);
 		}
 
-		if (jCurrentIt == jNumbers.end() - 1) break;
-		jPreviousIt = jPreviousIt + 1;
-		jCurrentIt = jCurrentIt + 1;
+		std::advance(jPreviousIt, 1);
+		std::advance(jCurrentIt, 1);
 	}
 
 	return indices;
 }
-
